355e8f793c8e1b079be60cf58a9d178bed077184
DKauff3r <danikf31@gmail.com>
2024-07-23 16:18:44 +0200

    ejercicio 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/DKauff3r.py
@@ -0,0 +1,30 @@
+# https://www.python.org/
+
+#comentario en una linea
+
+"""
+comentario en varias lineas
+"""
+
+'''
+comentario en varias lineas
+'''
+
+my_variable = "Hola Mundo"
+my_variable = "Hola Python" # Modifico el valos de my_variable
+
+MY_CONSTANT = "Mi constante" # por convención, en python no existe las constantes
+
+#Datos primitivos en python
+my_int = 1 #Numero entero
+my_float = 1.5 #Numero flotante/con decimales
+my_bool = True #Booleano
+my_bool = False #Booleano
+my_string = "Mi cadena de texto" #Cadena de texto
+my_other_string = 'Mi otra cadena de texto' #Cadena de texto
+
+print("¡Hola, Python!")
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
